9c6aa215d67549c4c1af4fff3315a8f2c5a9803f
Habib <[hbtonoy42@gmail.com]>
2021-11-21 17:29:42 +0600

    assigned api build

--- app/Http/Controllers/BillStatusController.php
+++ app/Http/Controllers/BillStatusController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\ImageUploads\Images;
 use App\Models\BillInfo;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -16,23 +18,22 @@ class BillStatusController extends Controller
             'agent_id' => 'required',
             'agent_name' => 'required'
         ]);
+        $file_handler = new Images();
+        $file_name = $request->telephone_number.'_'.rand(10000,99999);
+        $image_file_path = $file_handler->uploadFile($request->file('bill_images'),$file_name);
 
-        if($request->hasfile('bill_images'))
-        {
 
-            foreach($request->file('bill_images') as $image)
-            {
-                $name=$image->getClientOriginalName();
-                $image->move(public_path().'/images/', $name);
-                $data[] = $name;
-            }
-        }
         $bill_created = BillInfo::create([
             'phone_number' => $request->telephone_number,
-            'bill_images'  => $data,
+            'bill_images' => $image_file_path,
             'agent_id' => $request->agent_id,
             'agent_name' => $request->agent_name,
             'status' => 'Assigned'
+        ]);
+        return response()->json([
+            "data" => $bill_created,
+            'message' => 'Assigned',
+            'status' => 200
         ]);
     }
 }
